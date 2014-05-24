36093fdaff77457d5c41dc712a21c377a17a0188
AlejandroFV <AlejandroFV@mail.com>
2014-05-24 00:17:59 -0500

    Agregado de funcionalidades a los botones

--- src/View/Frame.java
+++ src/View/Frame.java
@@ -6,6 +6,13 @@
 
 package View;
 
+import Controller.analizadorControl;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Alejandro
@@ -30,31 +37,41 @@ public class Frame extends javax.swing.JFrame {
 
         jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
+        textAreaEntrada = new javax.swing.JTextArea();
         jLabel2 = new javax.swing.JLabel();
         jScrollPane2 = new javax.swing.JScrollPane();
-        jTextArea2 = new javax.swing.JTextArea();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        textAreaSalida = new javax.swing.JTextArea();
+        parseButton = new javax.swing.JButton();
+        clearButton = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Pascal Parser");
 
         jLabel1.setText("Entrada:");
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
+        textAreaEntrada.setColumns(20);
+        textAreaEntrada.setRows(5);
+        jScrollPane1.setViewportView(textAreaEntrada);
 
         jLabel2.setText("Resultado:");
 
-        jTextArea2.setColumns(20);
-        jTextArea2.setRows(5);
-        jScrollPane2.setViewportView(jTextArea2);
+        textAreaSalida.setColumns(20);
+        textAreaSalida.setRows(5);
+        jScrollPane2.setViewportView(textAreaSalida);
 
-        jButton1.setText("Analizar");
+        parseButton.setText("Analizar");
+        parseButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                parseButtonActionPerformed(evt);
+            }
+        });
 
-        jButton2.setText("Borrar");
+        clearButton.setText("Borrar");
+        clearButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                clearButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -71,9 +88,9 @@ public class Frame extends javax.swing.JFrame {
                             .addComponent(jLabel2))
                         .addGap(0, 0, Short.MAX_VALUE))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jButton1)
+                        .addComponent(parseButton)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jButton2)))
+                        .addComponent(clearButton)))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -85,8 +102,8 @@ public class Frame extends javax.swing.JFrame {
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 195, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jButton2)
-                    .addComponent(jButton1))
+                    .addComponent(clearButton)
+                    .addComponent(parseButton))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -96,6 +113,27 @@ public class Frame extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void clearButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_clearButtonActionPerformed
+        // TODO add your handling code here:
+        textAreaEntrada.setText(null);
+        textAreaSalida.setText(null);
+    }//GEN-LAST:event_clearButtonActionPerformed
+
+    private void parseButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_parseButtonActionPerformed
+        // TODO add your handling code here:
+        File fichero = new File("salida.txt");
+        BufferedWriter writer;
+        try {
+            writer = new BufferedWriter(new FileWriter(fichero));
+            writer.write(textAreaEntrada.getText());
+            writer.close();
+        } catch (IOException ex) {
+            JOptionPane.showMessageDialog(null, "No se pudo guardar archivo de entrada debido a " + ex.toString());
+        }
+        analizadorControl a = new analizadorControl();
+        textAreaSalida.setText(a.parsear());
+    }//GEN-LAST:event_parseButtonActionPerformed
 
     /**
      * @param args the command line arguments
@@ -133,13 +171,13 @@ public class Frame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
+    private javax.swing.JButton clearButton;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTextArea jTextArea1;
-    private javax.swing.JTextArea jTextArea2;
+    private javax.swing.JButton parseButton;
+    private javax.swing.JTextArea textAreaEntrada;
+    private javax.swing.JTextArea textAreaSalida;
     // End of variables declaration//GEN-END:variables
 }
